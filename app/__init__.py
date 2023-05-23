9f30f09665b7119fb3a013f22488497d0c121daa
Allam9573 <aallamdaniel@gmail.com>
2023-05-23 15:31:53 -0600

    creado error404.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 
+
 def create_app():
     app = Flask(__name__)
+
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.errorhandler(404)
+    def error404(error):
+        return render_template('error404.html')
     return app
